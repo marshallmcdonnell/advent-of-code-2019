c4af95f2626dcd2ebf8ce5f06ce9dbc43c921bae
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-12-02 10:06:08 -0500

    Added code, tests, and README for Day 1 - Part II

--- day_1/spacecraft.py
+++ day_1/spacecraft.py
@@ -13,10 +13,37 @@ def fuel_counter_upper(mass):
     :return: The fuel requirement for the input mass.
     :rtype: float
     '''
-    return math.floor(mass / 3) - 2
+    fuel = math.floor(mass / 3) - 2
+    if fuel < 0:
+        fuel = 0
+    return fuel
 
 
-def fuel_counter_upper_summation(masses):
+def fuel_counter_upper_recursive(mass, total_fuel=0):
+    '''
+    Determine fuel required for a module given its mass. Specifically,
+    will divide mass by 3, round down, and subtract by 2. Also,
+    calculate the additional fuel for the additional fuel's mass.
+    Recursively calculates till we reach a infinitesimal mass of fuel.
+
+
+    :param mass: Mass of the module to calculate fuel for
+    :type mass: float
+    :param total_fuel: Running total of fuel requirement (default=0)
+    :type total_fuel: float
+    :return: The fuel requirement for the input mass.
+    :rtype: float
+    '''
+    fuel = fuel_counter_upper(mass)
+    total_fuel += fuel
+
+    if fuel <= 0:
+        return total_fuel
+    else:
+        return fuel_counter_upper_recursive(fuel, total_fuel)
+
+
+def fuel_counter_upper_summation(masses, recursive=False):
     '''
     Determine fuel required for a module given its mass. Specifically,
     will divide mass by 3, round down, and subtract by 2.
@@ -24,12 +51,19 @@ def fuel_counter_upper_summation(masses):
 
     :param mass: Mass of the module to calculate fuel for
     :type mass: float
+    :param recursive: Boolean flag to run recursive version
+                      for accounting for additional fuel's mass.
+    :type recursive: bool
     :return: The fuel requirement for the input mass.
     :rtype: float
     '''
     output = []
     for mass in masses:
-        fuel = fuel_counter_upper(mass)
+        if recursive:
+            fuel = fuel_counter_upper_recursive(mass)
+        else:
+            fuel = fuel_counter_upper(mass)
+
         output.append(fuel)
 
     return sum(output)
@@ -37,13 +71,22 @@ def fuel_counter_upper_summation(masses):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("input_file",
+    parser.add_argument("input_file", type=str,
                         help="Input file with list of module masses.")
+    parser.add_argument("--part", type=int, default=1,
+                        choices=[1, 2],
+                        help="Either sovling part 1 or part 2 of problem")
     args = parser.parse_args()
 
     with open(args.input_file, 'r') as f:
         contents = f.readlines()
 
     masses = [float(x.strip()) for x in contents]
-    fuel = fuel_counter_upper_summation(masses)
+
+    if args.part == 1:
+        recursive = False
+    elif args.part == 2:
+        recursive = True
+
+    fuel = fuel_counter_upper_summation(masses, recursive=recursive)
     print("Total fuel required: {}".format(fuel))
--- day_1/test_spacecraft.py
+++ day_1/test_spacecraft.py
@@ -8,5 +8,21 @@ def test_fuel_counter_upper():
     assert spacecraft.fuel_counter_upper(100756) == 33583
 
 
+def test_fuel_counter_upper_recursive():
+    assert spacecraft.fuel_counter_upper_recursive(12) == 2
+    assert spacecraft.fuel_counter_upper_recursive(14) == 2
+    assert spacecraft.fuel_counter_upper_recursive(1969) == 966
+    assert spacecraft.fuel_counter_upper_recursive(100756) == 50346
+
+
 def test_fuel_counter_upper_summation():
-    assert spacecraft.fuel_counter_upper_summation([12, 14]) == 2 + 2
+    masses = [12, 14, 1969, 100756]
+    target = 2 + 2 + 654 + 33583
+    assert spacecraft.fuel_counter_upper_summation(masses) == target
+
+
+def test_fuel_counter_upper_summation_recursive():
+    masses = [12, 14, 1969, 100756]
+    target = 2 + 2 + 966 + 50346
+    assert spacecraft.fuel_counter_upper_summation(
+        masses, recursive=True) == target
